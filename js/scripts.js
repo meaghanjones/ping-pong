167a8863b788b3a136462a44e02b9236e0b5acbd
meaghanjones <meaghan.m.jones@gmail.com>
2016-06-14 15:50:52 -0700

    empty html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,49 @@
-/*This file is for your custom js.  All yours*/
+
+//business logic
+var pingPongNumbers = function(input) {
+  var pingPongArray =[];
+  if ($("#number-input").val() <= 0){
+
+  alert("Enter a positive number greater than 0!");
+  return;
+  }
+
+  else {
+   for (i=1; i<=input; i +=1){
+
+    if ((i%3 !=0) && (i%5 !=0)){
+      pingPongArray.push(i);
+    }
+    else if ((i%15===0)){
+    pingPongArray.push("pingpong");
+
+    }
+    else if (i%5===0){
+     pingPongArray.push("pong");
+    }
+    else if (i%3===0){
+    pingPongArray.push("ping");
+    }
+
+  }
+  return pingPongArray;
+  }
+
+  }
+
+//user interface
+
+$(document).ready( function() {
+  $("form#blank-number").submit(function(event) {
+    event.preventDefault();
+    $('#output').empty();
+  var potentialPingPong = parseInt($("#number-input").val());
+  var answer = pingPongNumbers(potentialPingPong);
+
+  if (answer === undefined || answer ===null){
+    return;
+  } else {
+    $("#output").append("<li>" + answer + "</li>");
+  }
+  });
+});
