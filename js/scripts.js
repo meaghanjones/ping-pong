4dfee1c517a0b61ae58880815eedb4b8c3984a37
meaghanjones <meaghan.m.jones@gmail.com>
2016-06-16 21:31:47 -0700

    change spacing in html and js files and append the list so it prints each item on its own line

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,33 @@
-
 //business logic
 var pingPongNumbers = function(input) {
   var pingPongArray =[];
-  if ($("#number-input").val() <= 0){
-
-  alert("Enter a positive number greater than 0!");
-  return;
-  }
-
-  else {
    for (i=1; i<=input; i +=1){
-
     if ((i%3 !=0) && (i%5 !=0)){
       pingPongArray.push(i);
-    }
-    else if ((i%15===0)){
+    } else if ((i%15===0)){
     pingPongArray.push("pingpong");
-
-    }
-    else if (i%5===0){
+    } else if (i%5===0){
      pingPongArray.push("pong");
-    }
-    else if (i%3===0){
+    } else if (i%3===0){
     pingPongArray.push("ping");
     }
-
   }
   return pingPongArray;
-  }
-
-  }
+}
 
 //user interface
-
 $(document).ready( function() {
   $("form#blank-number").submit(function(event) {
     event.preventDefault();
     $('#output').empty();
   var potentialPingPong = parseInt($("#number-input").val());
   var answer = pingPongNumbers(potentialPingPong);
-
-  if (answer === undefined || answer ===null){
-    return;
+  if (answer === undefined || answer ===null || (answer <=0)){
+    alert ("Please enter a positive number greater than 0");
   } else {
-    $("#output").append("<li>" + answer + "</li>");
+    answer.forEach (function (item){
+      $("#output").append("<li>" + item + "</li>");
+  })
   }
   });
 });
